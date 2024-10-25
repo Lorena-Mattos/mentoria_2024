4e9adca1bee231c6e9f49e307f9788275f7a8cd7
Lorena <lorena.mattos6@gmail.com>
2024-10-25 15:26:20 -0300

    create example of test

---
+++ testes/test_smoke.py
@@ -0,0 +1,28 @@
+import pytest
+from worker import Worker
+
+
+def test_smoke():
+    """ Testing the instance of Worker class """
+    worker = Worker()
+    assert isinstance(worker, Worker)
+
+
+def test_methods_smoke():
+    """ Testing if worker. transform is a method """
+    worker = Worker()
+    assert hasattr(worker.transform, '__call__')
+
+
+def test_transform():
+    """ Testing if the result of transform method is equal 1 """
+    worker = Worker()
+    result = worker.transform(value='1.0', convert_to='int')
+    assert result == 1
+
+
+def test_transform_with_exception():
+    """ Testing if the method raise an Exception """
+    worker = Worker()
+    with pytest.raises(ValueError):
+        worker.transform(value='1.0', convert_to='invalid_value')
